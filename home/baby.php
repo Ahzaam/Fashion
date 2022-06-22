0e8658b54fc3e9da29fd15072f2062b4fc93d5a9
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-22 15:54:58 +0530

    update

--- home/baby.php
+++ home/baby.php
@@ -25,9 +25,12 @@ session_start(); ?>
 
     <!-- bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../files/css/bootstrap.css">
-    <script type="text/javascript" src="../files/js/bootstrap.bundle.js"></script>
-
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <!-- Grid -->
     <link rel="stylesheet" href="../files/grid/grid.css">
     <style media="screen">
@@ -45,96 +48,54 @@ session_start(); ?>
       height:auto;
 
     }
+    .allcontain{
+      max-width:1800px;
+    }
+    body{
+      text-align:center;
+    }
+
     </style>
 
   </head>
 
 
   <body>
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light" style="">
-
-  <div class="container-fluid" >
-
-        <a href="#" >
-          <img src="../bg-images/logo.png" style="" type="logo" alt=""><span class="logo-txt d-none d-lg-inline-block d-md-inline-block" style="color:#00f7ff; margin-left:20px;">Mega</span>
-          <span class="logo-txt d-none d-lg-inline-block d-md-inline-block" style="color:#ff00e6;">Trons</span>
-         </a>
-
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon" ></span>
+    <div class="container-fluid allcontain">
+    
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item activenav ms-5">
-          <a class="nav-link " aria-current="page" href="../home.php">Home</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item ms-5" ><!--style="background:linear-gradient(90deg, #0affda, #ff71e4);border-radius:25px;"-->
-          <a class="nav-link activenavp" href="#">Baby Conner
-          </a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
         </li>
-
-        <li class="nav-item dropdown activenav ms-5">
-          <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Drop Down
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
           </a>
-          <ul class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-
-        <li class="nav-item activenav ms-5">
-          <a class="nav-link " href="#">Pricing</a>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
         </li>
-        <a href="../login/login.php?page=../baby-corner"><button class="btn btn-outline-success btn-lg ms-5 "  style="<?php echo ($_SESSION['login'])?'display:none;':''; ?> width:180px;" type="submit">Login</button></a>
-
-                <?php
-                if($_SESSION['login']){
-
-                  ?>
-
-                          <li class="nav-item dropdown ms-5" style="border:4px solid #75ffea ; ">
-                            <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style='color:#ff00e6;font-size:24px; text-align:center; '>
-                              <?php echo ucfirst($_SESSION['uname']) ?>
-                            </a>
-                            <ul class="dropdown-menu " style="background-color:rgba(255, 245, 255, 0.9);" aria-labelledby="navbarDropdownMenuLink">
-                              <!--  -->
-
-                              <?php
-                                if($_SESSION['profile'] == NULL){
-                                  $arr = str_split($_SESSION['uname']);
-                                  ?>
-
-                                    <li style="background-color:rgba(5, 255, 0, 0.6);margin:auto; width: 80px;height:80px;padding-top:12px; border-radius:40px; text-align:center;margin-bottom: 20px;"> <a href="../profile"><h1><?php echo strtoupper($arr[0]) ;?></h1></a></li>
-                                  <?php
-                                }else{
-                                  ?>
-
-                                  <li > <a href="../profile"><img    type="profile" src="../profile/<?php echo $_SESSION['profile'] ?>" alt="<?php echo $_SESSION['profile'] ?>" class="dropdown-item"></a> <li>
-
-                                    <?php
-                                }
-
-                               ?>
-
-                              <li><a class="dropdown-item" style="color:black; font-size:12px;"href="http://gmail.com?email=<?php echo  $_SESSION['email'];?>"><?php echo $_SESSION['email'] ?></a></li>
-                              <li><a class="dropdown-item" style="color:red;"href="../login/logout.php?page=../baby-corner/">Logout</a></li>
-
-                            </ul>
-                          </li>
-
-
-                  <?php
-                }
-                 ?>
-
       </ul>
-      <br><br><br>
-
-      <br><br>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </nav>
@@ -690,7 +651,8 @@ session_start(); ?>
     </div>
   </div>
 </div>
-
+    
+  </div>
 <script>
 $('.owl-carousel').owlCarousel({
   loop:true,
